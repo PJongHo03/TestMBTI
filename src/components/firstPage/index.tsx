6355de2422b7678038abfa7231282e138e8bc517
PJongHo03 <ghdls1254@gmail.com>
2025-08-13 19:08:01 +0900

    Icon-Component

--- src/components/firstPage/index.tsx
+++ src/components/firstPage/index.tsx
@@ -2,15 +2,14 @@ import * as S from "./first-page.styles";
 import CardGroup from "./Card/Card-Group";
 
 import StartButton from "./StartButton/Start-Button";
+import BrainIcon from "../../icon/BrainIcon";
+import { Brain } from "lucide-react";
 
 export default function FirstPage() {
   return (
     <S.Background>
       <S.Header>
-        <S.Circle>
-          <S.BrainIcon />
-        </S.Circle>
-
+        <BrainIcon width={80} height={80} iconSize={45} icon={<Brain />} />
         <S.Title>간단한 MBTI 테스트</S.Title>
         <S.SubTitle>20개의 질문으로 당신의 성격 유형을 알아보세요</S.SubTitle>
       </S.Header>
---
+++ src/icon/BrainIcon.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { ReactNode } from "react";
+import { icons } from "lucide-react";
+
+type BrainIconProps = {
+  width: number;
+  height: number;
+  iconSize: number;
+  icon: React.ReactNode;
+};
+
+type IconBoxProps = {
+  iconSize: number;
+};
+
+type CircleProps = {
+  width: number;
+  height: number;
+};
+
+export const Circle = styled.div<CircleProps>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center; // 가로 가운데
+  align-items: center; // 세로 가운데
+  background: linear-gradient(to bottom right, #8578f7, #3d9df6);
+`;
+
+export const Icon = styled.div<IconBoxProps>`
+  svg {
+    display: block;
+    height: ${(porps) => porps.iconSize}px;
+    color: #ffffff;
+  }
+`;
+
+export default function BrainIcon({
+  width,
+  height,
+  iconSize,
+  icon,
+}: BrainIconProps) {
+  return (
+    <Circle width={width} height={height}>
+      <Icon iconSize={iconSize}>{icon}</Icon>
+    </Circle>
+  );
+}
