dd279a9b2b5ade079d45afc9c0c8602bebd7b5ee
PJongHo03 <ghdls1254@gmail.com>
2025-08-27 02:02:10 +0900

    result

--- src/components/resultPage/index.tsx
+++ src/components/resultPage/index.tsx
@@ -5,10 +5,21 @@ import { ContextMBTI } from "../../store/mbti-context";
 export default function ResultPage() {
   const { mbtiResult } = useContext(ContextMBTI);
 
-  console.log(mbtiResult);
+  const getMbtiString = (scores: number[]): string => {
+    const { energy, information, decision, lifeStyle } = scores;
+
+    const EorI = energy >= 0 ? "E" : "I";
+    const NorS = information >= 0 ? "N" : "S";
+    const ForT = decision >= 0 ? "F" : "T";
+    const PorJ = lifeStyle >= 0 ? "P" : "J";
+
+    return `${EorI}${NorS}${ForT}${PorJ}`;
+  };
+
+  const result = getMbtiString(mbtiResult);
   return (
     <S.Background>
-      <h1>결과</h1>
+      <h1>{result}</h1>
     </S.Background>
   );
 }
