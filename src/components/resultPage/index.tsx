8ae8064c563cb0fd687e507115c5916d116a4469
PJongHo03 <ghdls1254@gmail.com>
2025-09-03 21:39:16 +0900

    feat : MBTI 결과 UI 추가

--- src/components/resultPage/index.tsx
+++ src/components/resultPage/index.tsx
@@ -1,13 +1,24 @@
 import { use, useContext } from "react";
 import * as S from "./result-page-styled";
 import { ContextMBTI } from "../../store/mbti-context";
+import BrainIcon from "../../icon/BrainIcon";
+import { mbti_Info } from "../../data";
+import type { MBTI, mbtiInfo } from "../../data";
+import { Brain } from "lucide-react";
 
 export default function ResultPage() {
   const { mbtiResult } = useContext(ContextMBTI)!;
 
+  const resultInfo: mbtiInfo = mbti_Info[mbtiResult as MBTI];
+
+  console.log(resultInfo);
+
   return (
     <S.Background>
-      <h1>{mbtiResult}</h1>
+      <BrainIcon width={60} height={60} icon={<Brain />} iconSize={40} />
+      <S.Section>
+        <S.SectionTitle>성격 분석</S.SectionTitle>
+      </S.Section>
     </S.Background>
   );
 }
