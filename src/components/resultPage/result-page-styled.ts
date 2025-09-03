9077fb37ab71c6f9ec76c009d18ccd4e530733b7
PJongHo03 <ghdls1254@gmail.com>
2025-09-03 21:37:49 +0900

    style : 결과 페이지 스타일 수정

--- src/components/resultPage/result-page-styled.ts
+++ src/components/resultPage/result-page-styled.ts
@@ -11,3 +11,20 @@ export const Background = styled.div`
   align-items: center; // 세로 가운데
   flex-direction: column;
 `;
+
+export const Section = styled.div`
+  margin-top: 20px;
+  margin-left: 51%;
+  background-color: #ffffff;
+  display: flex;
+  width: 450px;
+  height: 380px;
+  justify-content: center; // 가로 가운데
+  border-radius: 10px;
+  box-shadow: 0 12px 24px rgba(0, 0, 0, 0.25);
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`;
