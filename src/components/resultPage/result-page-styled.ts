7f7f9dd20d57575b4411e844e42376da9b6da39e
PJongHo03 <ghdls1254@gmail.com>
2025-09-04 15:22:42 +0900

    style: mbti결과 추가

--- src/components/resultPage/result-page-styled.ts
+++ src/components/resultPage/result-page-styled.ts
@@ -14,17 +14,67 @@ export const Background = styled.div`
 
 export const Section = styled.div`
   margin-top: 20px;
-  margin-left: 51%;
-  background-color: #ffffff;
   display: flex;
-  width: 450px;
-  height: 380px;
-  justify-content: center; // 가로 가운데
-  border-radius: 10px;
-  box-shadow: 0 12px 24px rgba(0, 0, 0, 0.25);
 `;
-
 export const SectionTitle = styled.h2`
   font-size: 24px;
   font-weight: bold;
 `;
+
+export const SectionGraph = styled.div`
+  background-color: #ffffff;
+  width: 450px;
+  height: 380px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center; // 가로 가운데
+  border-radius: 10px;
+  box-shadow: 0 12px 24px rgba(0, 0, 0, 0.25);
+  margin-bottom: 20px;
+`;
+
+export const ResultSection = styled.div`
+  width: 400px;
+  height: 100px;
+  margin-left: 50px;
+  margin-right: 64px;
+  display: flex;
+  align-items: center; // 세로 가운데
+
+  flex-direction: column;
+`;
+
+export const ResultSpan = styled.div`
+  display: flex;
+  justify-content: center; // 가로 가운데
+  gap: 28px;
+`;
+type props = {
+  color: string;
+};
+
+export const ResultCode = styled.div<props>`
+  background-color: ${({ color }) => color};
+  font-size: 32px;
+  color: #ffffff;
+  width: 200px;
+  height: 60px;
+  display: flex;
+  text-align: center;
+  justify-content: center; // 가로 가운데
+  align-items: center; // 세로 가운데
+  border-radius: 10px;
+  margin-bottom: 12px;
+`;
+export const ResultTitle = styled.div<props>`
+  font-size: 32px;
+  color: ${({ color }) => color};
+  font-weight: bold;
+`;
+
+export const ResultMessage = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  color: #878787;
+`;
+export const ResultButton = styled.button``;
