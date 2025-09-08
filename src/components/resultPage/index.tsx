d71d4cf568095ff05598ed537dcbceded8c357cc
kiopc7 <ghdls1254@gmail.com>
2025-09-08 14:48:18 +0900

    feat: component 분리

--- src/components/resultPage/index.tsx
+++ src/components/resultPage/index.tsx
@@ -5,38 +5,19 @@ import BrainIcon from "../../icon/BrainIcon";
 import { mbti_Info } from "../../data";
 import type { MBTI, mbtiInfo } from "../../data";
 import { Brain } from "lucide-react";
+import ResultSection from "./resultSection/ResultSection";
 
 export default function ResultPage() {
   const { mbtiResult } = useContext(ContextMBTI)!;
 
   const resultInfo: mbtiInfo = mbti_Info[mbtiResult as MBTI];
 
-  const [resultInfoCode, resultInfoTitle, resultInfoMessage, resultColor] = [
-    resultInfo.code,
-    resultInfo.title,
-    resultInfo.message,
-    resultInfo.color,
-  ];
-
   return (
     <>
       <S.Background>
         <BrainIcon width={60} height={60} icon={<Brain />} iconSize={40} />
-
         <S.Section>
-          <S.ResultSection>
-            <S.ResultSpan>
-              <S.ResultCode color={resultInfo.color}>
-                {resultInfoCode}
-              </S.ResultCode>
-              <S.ResultTitle color={resultInfo.color}>
-                {resultInfoTitle}
-              </S.ResultTitle>
-            </S.ResultSpan>
-            <S.ResultMessage>{resultInfoMessage}</S.ResultMessage>
-
-            <S.ReplayButton>다시 하기</S.ReplayButton>
-          </S.ResultSection>
+          <ResultSection />
 
           <S.SectionGraph>
             <S.SectionTitle>성격 분석</S.SectionTitle>
