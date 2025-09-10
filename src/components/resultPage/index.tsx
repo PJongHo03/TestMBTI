5ca998006a12539991eb85e226e49ffd05f5be04
kiopc7 <ghdls1254@gmail.com>
2025-09-10 15:29:37 +0900

    refactor: 사용 안하는 코드 삭제

--- src/components/resultPage/index.tsx
+++ src/components/resultPage/index.tsx
@@ -1,18 +1,10 @@
-import { use, useContext } from "react";
 import * as S from "./result-page-styled";
-import { ContextMBTI } from "../../store/mbti-context";
 import BrainIcon from "../../icon/BrainIcon";
-import { mbti_Info } from "../../data";
-import type { MBTI, mbtiInfo } from "../../data";
 import { Brain } from "lucide-react";
 import ResultSection from "./ResultSection";
 import GraphSection from "./GraphSection";
 
 export default function ResultPage() {
-  const { mbtiResult } = useContext(ContextMBTI)!;
-
-  const resultInfo: mbtiInfo = mbti_Info[mbtiResult as MBTI];
-
   return (
     <>
       <S.Background>
